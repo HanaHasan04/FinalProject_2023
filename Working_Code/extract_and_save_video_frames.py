e3530822cd91a3efd0b5c0bf55979b21aba3f041
HanaHasan <100927079+HanaEtgar@users.noreply.github.com>
2022-12-29 15:55:27 +0200

    Update extract_and_save_video_frames.py

--- Working_Code/extract_and_save_video_frames.py
+++ Working_Code/extract_and_save_video_frames.py
@@ -2,12 +2,15 @@ import os
 from pathlib import Path
 import cv2
 
+# each frame is saved as its video name + its index in the video
+
 def extract_and_save_video_frames():
     path = Path('C:/Users/USER/Documents/UniversityProjects/PythonProjects/FinalProject')
     videos_path = Path(os.path.join(path, 'Videos')).glob('*')
+    images_path = os.path.join(path, 'Images')
+    
     for video_folder in videos_path:
         emotion = os.path.basename(video_folder)
-        images_path = os.path.join(path, 'Images')
         for video in os.listdir(video_folder):
             video_name = os.path.splitext(video)[0]
             tmp = os.path.join(os.path.join(os.path.join(os.path.join(path, "Videos"), emotion)), str(video_name)+".mp4")
