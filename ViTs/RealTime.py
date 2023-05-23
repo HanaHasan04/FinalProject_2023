5f45fcaf3e196282e4e3380597810d33f48d4ab5
Hana Hasan <100927079+HanaHasan04@users.noreply.github.com>
2023-05-23 09:09:10 +0300

    Create RealTime.py

---
+++ ViTs/RealTime.py
@@ -0,0 +1,72 @@
+import cv2
+import torch
+import pandas as pd
+from torch import IntTensor
+from PIL import Image
+from torchvision.transforms import functional as F
+from sklearn.naive_bayes import GaussianNB
+
+# Load the trained Naive Bayes model
+def load_naive_bayes_model(model_path):
+    model = GaussianNB()
+    model.fit(X_train, y_train)  
+    return model
+
+def load_yolo_model():
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+    return model
+
+def process_video(video_path, nb_model, yolo_model, excel_path):
+    df = pd.read_excel(excel_path)  
+    cap = cv2.VideoCapture(video_path)
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        pil_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+        results = yolo_model(pil_image)
+
+        # Format of the results: a table where each row is an object,
+        # columns: xmin, ymin, xmax, ymax, confidence, class
+        v = results.xyxy[0]
+
+        horse_class = 17
+        for i in range(v.shape[0]):
+            if IntTensor.item(v[i][5]) == horse_class:
+                xmin = int(IntTensor.item(v[i][0]))
+                ymin = int(IntTensor.item(v[i][1]))
+                xmax = int(IntTensor.item(v[i][2]))
+                ymax = int(IntTensor.item(v[i][3]))
+
+                crop_im = frame[ymin:ymax, xmin:xmax]
+                small_im = cv2.resize(crop_im, (0, 0), fx=0.5, fy=0.5)
+
+                pil_crop = Image.fromarray(cv2.cvtColor(small_im, cv2.COLOR_BGR2RGB))
+
+                features = extract_features(pil_crop)
+
+                prediction = nb_model.predict([features])[0]
+
+                cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+                cv2.putText(frame, prediction, (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+def extract_features(image):
+  pass  
+
+naive_bayes_model = load_naive_bayes_model("path_to_naive_bayes_model")
+yolo_model = load_yolo_model()
+excel_path = "path_to_excel_file"
+video_path = "path_to_video_file"
+
+process_video(video_path, naive_bayes_model, yolo_model, excel_path)
